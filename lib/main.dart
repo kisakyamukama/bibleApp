6f6cfc46a872ed26d754524f238d8bef442d644b
Michael <michael.kabali@sybyl.com>
2021-11-19 08:46:36 +0300

    fix error with Psalms Psalm and add app color conforming to the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:bible_app/core/provider/bible.provider.dart';
-import 'package:bible_app/core/utility/config.dart';
 import 'package:bible_app/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -12,6 +12,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Theme.of(context).scaffoldBackgroundColor,
+      statusBarColor:  Theme.of(context).scaffoldBackgroundColor, //Theme.of(context).scaffoldBackgroundColor,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    // For iOS.
+    // Use [dark] for white status bar and [light] for black status bar.
+    statusBarBrightness: Brightness.dark,
+    ));
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create:(_) => BibleModel())
